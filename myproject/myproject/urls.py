af2f2d64e0bd3814239a09d3e3388ee90b2f23a0
Amin-mashari <amin0916.m@gmail.com>
2023-01-29 17:06:17 +0330

    set static path or media

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('',include('blog.urls'))
 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
