86edb078911af1d457411ab136e11cdeb44fb20e
Amin-mashari <amin0916.m@gmail.com>
2023-01-29 18:10:42 +0330

    reading data in view from database and set orderby for view in blog

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse as HR
-from django.http import JsonResponse as JR
+# from django.http import HttpResponse as HR
+# from django.http import JsonResponse as JR
+from .models import Article
 
 # Create your views here.
 
 
 def home(request):
     context = {
-        'animals': [
-            {'name': "dog",
-             'img': 'https://en.wiktionary.org/wiki/dog#/media/File:YellowLabradorLooking.jpg'
-             },
-            {'name': "cat",
-             'img': 'https://en.wikipedia.org/wiki/Cat#/media/File:Felis_catus-cat_on_snow.jpg'
-             }
-        ]
+        'articles': Article.objects.filter(status='p').order_by('-publish')
     }
     return render(request, 'blog/home.html', context)
