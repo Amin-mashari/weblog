f01636085584c37bd3999cbf246b60799fa23ca5
Amin-mashari <amin0916.m@gmail.com>
2023-01-30 15:53:12 +0330

    add ArticleAdmin to admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Article
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+     list_display = ('title','slug','publish','status')
+     list_filter = ('publish','status')
+     search_fields = ('title','description')
+     prepopulated_fields = {'slug':('title',)}
+     ordering = ['-status','-publish']
 
 
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
