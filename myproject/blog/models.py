7e7dcf27f1789357db251e503a4af92c8a3fbd66
Amin-mashari <amin0916.m@gmail.com>
2023-01-30 14:40:12 +0330

    chnage admin panel to persian

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -4,18 +4,22 @@ from django.utils import timezone
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to='images')
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200,verbose_name ='عنوان مقاله')
+    slug = models.SlugField(max_length=100, unique=True,verbose_name ='آدرس مقاله')
+    description = models.TextField(verbose_name ='محتوا')
+    thumbnail = models.ImageField(upload_to='images',verbose_name ='تصویر مقاله')
+    publish = models.DateTimeField(default=timezone.now,verbose_name ='زمان انتشار')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     STATUS_CHOICES = (
-        ('d', 'DRAFT'),
-        ('p', 'PUBLISHED'),
+        ('d', 'پیش‌نیوس'),
+        ('p', 'منتشر شده'),
     )
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,verbose_name ='وضعیت')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
 
     def __str__(self):
         return self.title
