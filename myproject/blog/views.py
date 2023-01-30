f61dd7973efd46e86f8c607e5e0428d28b508af8
Amin-mashari <amin0916.m@gmail.com>
2023-01-30 13:33:29 +0330

    change orderby view for website

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -8,7 +8,7 @@ from .models import Article
 
 def home(request):
     context = {
-        'articles': Article.objects.filter(status='p').order_by('-publish')
+        'articles': Article.objects.filter(status='p').order_by('publish')
     }
     return render(request, 'blog/home.html', context)
 
