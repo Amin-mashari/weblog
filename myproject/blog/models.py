0d38509f6a5cf128e316acda117b2a1835d2312f
Amin-mashari <amin0916.m@gmail.com>
2023-01-30 17:56:44 +0330

    set category to blog

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -2,11 +2,25 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_convertor
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200,verbose_name ='عنوان دسته بندی')
+    slug = models.SlugField(max_length=100, unique=True,verbose_name ='آدرس دسته بندی')
+    status = models.BooleanField(default=True,verbose_name ='ایا نمایش داده شود ؟')
+    position = models.IntegerField(verbose_name='پوزیشن')
+
+    class Meta:
+        verbose_name = 'دسته‌بندی'
+        verbose_name_plural = 'دسته‌بندی ها'
+        ordering = ['position']
+    
+    def __str__(self):
+        return self.title
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200,verbose_name ='عنوان مقاله')
     slug = models.SlugField(max_length=100, unique=True,verbose_name ='آدرس مقاله')
+    category = models.ManyToManyField(Category , verbose_name="دسته‌بندی")    
     description = models.TextField(verbose_name ='محتوا')
     thumbnail = models.ImageField(upload_to='images',verbose_name ='تصویر مقاله')
     publish = models.DateTimeField(default=timezone.now,verbose_name ='زمان انتشار')
