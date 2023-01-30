ca1802f64f90c43d0457e7846040cc67fce574a2
Amin-mashari <amin0916.m@gmail.com>
2023-01-30 15:54:19 +0330

    convert date to localtime

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from extensions.utils import jalali_convertor
 # Create your models here.
 
 
@@ -23,3 +24,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def jpublish(self):
+        return jalali_convertor(self.publish)
+
+    jpublish.short_description = 'زمان انتشار'
