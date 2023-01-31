5993d86df07f67a7eca4120a3a58731dd68360cb
Amin-mashari <amin0916.m@gmail.com>
2023-01-31 20:52:52 +0330

    add pagination to category

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('page/<int:page>', home, name='home'),
     path('article/<slug:slug>', detail, name='detail'),
-    path('category/<slug:slug>', category, name='category')
+    path('category/<slug:slug>', category, name='category'),
+    path('category/<slug:slug>/page/<int:page>',category, name='category')
 ]
