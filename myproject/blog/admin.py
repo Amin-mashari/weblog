09c5c1d77d79bbc38f9747c2a3f6f146217660c3
Amin-mashari <amin0916.m@gmail.com>
2023-01-31 20:51:56 +0330

    add parent fild to admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Article,Category
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
-     list_display = ('position','title','slug','status')
+     list_display = ('position','title','slug','parent','status')
      list_filter = (['status'])
      search_fields = ('title','slug')
      prepopulated_fields = {'slug':('title',)}
