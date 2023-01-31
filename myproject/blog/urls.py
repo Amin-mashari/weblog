fdadaf57e6a2628cb5262a22beee2dab9e8b5f0a
Amin-mashari <amin0916.m@gmail.com>
2023-01-31 18:27:27 +0330

    simplify page url

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('page/<int:page>', home, name='home'),
     path('article/<slug:slug>', detail, name='detail'),
     path('category/<slug:slug>', category, name='category')
 ]
