4cc576ae661e0be573d2f76b5f443cf3294ee96e
Amin-mashari <amin0916.m@gmail.com>
2023-02-01 17:09:54 +0330

    add actions to admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,11 +1,34 @@
 from django.contrib import admin
 from .models import Article,Category
+#actions
+def make_published(modeladmin, request, queryset):
+     row_updated = queryset.update(status='p')
+
+     message = "شدند"
+     if row_updated == 1:
+          message = "شد"  
+
+     modeladmin.message_user(request,'{} مقاله منتشر {}'.format(row_updated, message))
+
+make_published.short_description = "انتشار مقالات انتخاب شده"
+
+def make_draft(modeladmin, request, queryset):
+     row_updated = queryset.update(status='d')
+     message = "شدند"
+     if row_updated == 1:
+          message = "شد"  
+     
+     modeladmin.message_user(request,'{}  مقاله پیش‌نویس {}'.format(row_updated, message))
+     
+make_draft.short_description = "پیش‌نویس مقالات انتخاب شده"
+
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
      list_display = ('position','title','slug','parent','status')
      list_filter = (['status'])
      search_fields = ('title','slug')
      prepopulated_fields = {'slug':('title',)}
+
 
 
 admin.site.register(Category,CategoryAdmin)
@@ -17,6 +40,8 @@ class ArticleAdmin(admin.ModelAdmin):
      search_fields = ('title','description')
      prepopulated_fields = {'slug':('title',)}
      ordering = ['-status','-publish']
+     actions = [make_published,make_draft]
+
 
      def category_to_str(self,obj):
           return ", ".join([category.title for category in obj.category_published() ])
