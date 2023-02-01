02e4e20c4a90632eae0b06fff8c01ee5f2be4780
Amin-mashari <amin0916.m@gmail.com>
2023-02-01 17:54:25 +0330

    add thumbnail to admin panel

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -35,7 +35,7 @@ admin.site.register(Category,CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-     list_display = ('title','slug','jpublish','status','category_to_str')
+     list_display = ('title','slug','thumbnail_tag','jpublish','status','category_to_str')
      list_filter = ('publish','status')
      search_fields = ('title','description')
      prepopulated_fields = {'slug':('title',)}
