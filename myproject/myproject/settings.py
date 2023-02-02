054a064f8db789087acb9e8e846161cd70ca201e
Amin-mashari <amin0916.m@gmail.com>
2023-02-02 22:06:56 +0330

    add few changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -126,3 +126,4 @@ MEDIA_ROOT =  BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#collectstatic for create file static like css and html
