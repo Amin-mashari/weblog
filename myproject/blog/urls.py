0e23add8cb4f967eb0208921032561c95ae4b55e
Amin-mashari <amin0916.m@gmail.com>
2023-02-02 22:05:18 +0330

    add ListView and DetailView

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import ArticleList, detail ,category
+from .views import ArticleList, ArticleDetail ,CategoryList
 #ctrl + shift + i
 app_name = 'blog'
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('page/<int:page>', ArticleList.as_view(), name='home'),
-    path('article/<slug:slug>', detail, name='detail'),
-    path('category/<slug:slug>', category, name='category'),
-    path('category/<slug:slug>/page/<int:page>',category, name='category')
+    path('article/<slug:slug>', ArticleDetail.as_view(), name='detail'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='category'),
+    path('category/<slug:slug>/page/<int:page>',CategoryList.as_view(), name='category')
 ]
+ 
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render,get_object_or_404
 # from django.http import HttpResponse as HR
 # from django.http import JsonResponse as JR
@@ -27,21 +28,45 @@ class ArticleList(ListView):
 
 
 
-def detail(request, slug):
-    context = {
-        'article': get_object_or_404(Article, slug=slug, status='p')
-    }
-    return render(request, 'blog/detail.html', context)
+# def detail(request, slug):
+#     context = {
+#         'article': get_object_or_404(Article, slug=slug, status='p')
+#     }
+#     return render(request, 'blog/detail.html', context)
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return  get_object_or_404(Article.objects.published(), slug=slug)
 
 
-def category(request,slug,page=1):
+
+# def category(request,slug,page=1):
     
-    category = get_object_or_404(Category, slug=slug, status=True)
-    article_list = category.articles.published()
-    paginator = Paginator(article_list , 1)
-    articles = paginator.get_page(page)
-    context = {
-        'category':category,
-        'articles':articles,
-    }
-    return render(request, 'blog/category.html', context)
+#     category = get_object_or_404(Category, slug=slug, status=True)
+#     article_list = category.articles.published()
+#     paginator = Paginator(article_list , 1)
+#     articles = paginator.get_page(page)
+#     context = {
+#         'category':category,
+#         'articles':articles,
+#     }
+#     return render(request, 'blog/category.html', context)
+
+class CategoryList(ListView):
+    # model = Article
+    # template_name = 'blog/home.html'
+
+    paginate_by = 1
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        global category
+        slug = self.kwargs.get('slug')
+        category = get_object_or_404(Category.objects.active(), slug=slug)
+        return  category.articles.published()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context
