21de40a0ce1a15cf93d7dd4bed1133b1d6daac4f
Amin-mashari <amin0916.m@gmail.com>
2023-02-03 16:28:07 +0330

    set new DIR for templates andd add account app to apps

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'extensions'
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
