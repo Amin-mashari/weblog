e373b89734505c58f80c1adccc53dbc2a02bf52d
Amin-mashari <amin0916.m@gmail.com>
2023-02-06 20:05:17 +0330

    set redirect in arthorsAcces and set in articlelist and articlecreate

--- myproject/account/views.py
+++ myproject/account/views.py
@@ -13,6 +13,7 @@ from .mixins import (
      FieldsMixin, 
      FormValidMixin, 
      AuthorAccessMixin,
+     AuthorsAccessMixin,
      SuperUserAccessMixin
 )
 
@@ -22,7 +23,7 @@ from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
-class ArticleList(LoginRequiredMixin,ListView):
+class ArticleList(AuthorsAccessMixin,ListView):
      template_name='registration/home.html'
 
      def get_queryset(self):
@@ -32,7 +33,7 @@ class ArticleList(LoginRequiredMixin,ListView):
                return Article.objects.filter(author=self.request.user)
 
 
-class ArticleCreate(LoginRequiredMixin, FormValidMixin, FieldsMixin, CreateView):
+class ArticleCreate(AuthorsAccessMixin, FormValidMixin, FieldsMixin, CreateView):
      model = Article
      template_name = 'registration/article-create-update.html'
 
