04feb4808083ab2eeecf84ae127f127b486825d1
Amin-mashari <amin0916.m@gmail.com>
2023-02-13 09:00:08 +0330

    make email unique for user

--- myproject/account/models.py
+++ myproject/account/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 
 # Create your models here.
 class User(AbstractUser):
+     email = models.EmailField(unique=True, verbose_name='ایمیل')
+
      is_author = models.BooleanField(default=False,verbose_name='وضعیت نویسنده')
      special_user = models.DateTimeField(default=timezone.now,verbose_name ='کاربر ویژه تا')
 
